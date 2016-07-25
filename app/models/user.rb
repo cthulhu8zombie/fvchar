be543cacbfd991ccf5bde81605b70ecf601c80c3
Robert Dempsey <r.j.dempsey@gmail.com>
2016-07-25 22:05:10 +0000

    chapter6 work

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ApplicationRecord
+    # name validations
+    validates :name, 
+      presence: true, 
+      length: { maximum: 50}
+
+    # email validations
+    # /	start of regex
+    #   \A	match start of a string
+    #   [\w+\-.]+	at least one word character, plus, hyphen, or dot
+    #   @	literal “at sign”
+    #   [a-z\d\-.]+	at least one letter, digit, hyphen, or dot
+    #   (\.[a-z\d\-]+)* a dot followed by at least one letter, digit, hyphen zero or more times
+    #   \.	literal dot
+    #   [a-z]+	at least one letter
+    #   \z	match end of a string
+    # /	end of regex
+    # i	case-insensitive
+    VALID_EMAIL_REGEX = %r{\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z}i
+    
+    validates :email,
+      presence: true,
+      length: { maximum: 255},
+      format: { with: VALID_EMAIL_REGEX }
+end
---
+++ test/models/user_test.rb
@@ -0,0 +1,62 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  def setup
+    @user_name = 'Fragrant Vagrant'
+    @user_email = 'fragrant.vagrant@gmail.com'
+    @user = User.new(name: @user_name, email: @user_email)
+  end
+  
+  test 'should be valid' do
+    assert @user.valid?
+  end
+  
+  test 'name should be present' do
+    @user.name = "        "
+    assert_not @user.valid?
+  end
+  
+  test 'email should be present' do
+    @user.email = "        "
+    assert_not @user.valid?
+  end
+  
+  test 'name should not be too long' do
+    # repeat a 51 times to make it too long
+    @user.name = 'a' * 51
+    assert_not @user.valid?
+  end
+  
+  test 'email should not be too long' do
+    # repeat a 256 times to make it too long
+    @user.email = 'a' * 244 + '@example.com'
+    assert_not @user.valid?
+  end
+  
+  test 'email validation should accept valid addresses' do
+    # create an array of strings using %w
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    # for each address in the array
+    valid_addresses.each do |valid_address|
+      # set our email to the address to check
+      @user.email = valid_address
+      # assert that with this email our user is still valid or throw informative message
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+  
+  test "email validation should reject invalid addresses" do
+    # create an array of strings using %w
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+    # for each address in the array
+    invalid_addresses.each do |invalid_address|
+      # set our email to the address to check
+      @user.email = invalid_address
+      # assert that with this email our user is not valid or throw informative message
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+end
